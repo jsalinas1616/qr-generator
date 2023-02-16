f9f83968d58b4df1b85dd446506222ddf57ca6b8
Julian Salinas <julian@vinneren.com.mx>
2023-02-15 20:14:27 -0600

    a

--- index.js
+++ index.js
@@ -3,34 +3,36 @@ var qrcode = require('yaqrcode');
 /**
  * Generate qr codes from a list of strings (a qr code for each string)
  */
- var qrcode = require('yaqrcode');
+var qrcode = require('yaqrcode');
 
- exports.handler = async (event) => {
-   const requestBody = JSON.parse(event.body);
+exports.handler = async (event) => {
+  const requestBody = JSON.parse(event.body);
 
-   if (event.httpMethod !== 'POST' || !requestBody.items) {
-     return {
-       statusCode: 400,
-       headers: {
-         "x-custom-header" : "alpha"
-       },
-       body: 'Malfofrmed request.'
-     }
-   }
+  console.log('podria ser?');
 
-    let responseBody = requestBody.items.map(e => ({
-      item: e,
-      qr: qrcode(e), 
-    }))
-
-    let response = {
-      statusCode: 200,
+  if (event.httpMethod !== 'POST' || !requestBody.items) {
+    return {
+      statusCode: 400,
       headers: {
-        "x-custom-header" : "alpha"
+        "x-custom-header": "alpha"
       },
-      body: JSON.stringify(responseBody)
-     };
+      body: 'Malfofrmed request.'
+    }
+  }
 
-     return response;
- };
+  let responseBody = requestBody.items.map(e => ({
+    item: e,
+    qr: qrcode(e),
+  }))
+
+  let response = {
+    statusCode: 200,
+    headers: {
+      "x-custom-header": "alpha"
+    },
+    body: JSON.stringify(responseBody)
+  };
+
+  return response;
+};
 
